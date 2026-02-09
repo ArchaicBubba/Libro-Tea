5fb47b32be18ef1b2e5f95fbdb66f824f55c20c1
Roman <roman.c.antonacci@gmail.com>
2026-02-08 20:22:27 -0500

    Changed Echo to sys_mes Function. Fixed typo with mp3 files

--- utils/metadata.py
+++ utils/metadata.py
@@ -1,4 +1,4 @@
-from . import account, libraryManagment, settings
+from . import account, settings
 from librofm.client import LibroFMClient
 from mutagen.mp4 import MP4
 from mutagen.mp3 import MP3
@@ -6,8 +6,7 @@ import os, json, requests, datetime, math
 
 # Downloads the cover from LibroFM. Saves in audiobook folder.
 def export_cover(title: str, coverURL:str, path:str) -> bool:
-    if settings.config["debug"]>=1:
-        print(f"DEBUG 1 - RUNNING - Starting cover download for \"{title}\", file path will be \"{path}/cover.jpg.\"")
+    settings.debug_mes(1, "Running", f"Starting cover download for \"{title}\", file path will be \"{path}/cover.jpg.\"")
 
     fullURL = f"https:{coverURL}"
     response = requests.get(fullURL)
@@ -15,23 +14,21 @@ def export_cover(title: str, coverURL:str, path:str) -> bool:
     if response.status_code == 200:
         with open(f"{path}/cover.jpg", "wb") as f:
             f.write(response.content)
-        if settings.config["debug"]>=1:
-            print(f"DEBUG 1 - SUCCESS - Cover for \"{title}\" downloaded!")
+
+        settings.debug_mes(1, "Success", f"Cover for \"{title}\" downloaded!")
         return True
     
     else:
-        print(f"SYS MES -  ERROR  - Unable to find \"{title}\"'s cover")
+        settings.debug_mes(0, "Error", f"Unable to find \"{title}\"'s cover")
         return False
 
 # creates a metadata Json for audiobookshelf. Saves in audiobook folder
 def export_metadata(audiobook: dict, path: str) -> bool:
-    if settings.config["debug"]>=1:
-        print(f"Debug 1 - RUNNING - Exporting Metadata for {audiobook.title}")
+    settings.debug_mes(1, "Running", f"Exporting Metadata for {audiobook.title}")
 
     # Stops if it cant find the specified book
     if not os.path.exists(path):
-        if settings.config["debug"]>=1:
-            print(f"Debug 1 - ERROR - Unable to export metadata for \"{audiobook.title}\"; file path \"{path}\" does not exist!")
+        settings.debug_mes(1, "Error", f"Unable to export metadata for \"{audiobook.title}\"; file path \"{path}\" does not exist!")
         return False
 
     # Gets Genres
@@ -76,14 +73,12 @@ def export_metadata(audiobook: dict, path: str) -> bool:
     with open(f"{path}/metadata.json", "w") as f:
         f.write(json.dumps(metadata, indent=2))
 
-    if settings.config["debug"]>=1:
-        print(f"Debug 1 - SUCCESS - Finished Metadata for \"{audiobook.title}\"")
+    settings.debug_mes(1, "Success", f"Finished Metadata for \"{audiobook.title}\"")
     return True
 
 # Changes mp3/m4b files names to their book names
 def rename_to_title(title: str, ignoredFiles: list, path: str) -> bool:
-    if settings.config["debug"]>=1:
-        print(f"DEBUG 1 - RUNNING - Renaming audiobook files for \"{title}\"")
+    settings.debug_mes(1, "Running", f"Renaming audiobook files for \"{title}\"")
 
     bookFiles = enumerate_audiobook_folder(path)
 
@@ -98,28 +93,23 @@ def rename_to_title(title: str, ignoredFiles: list, path: str) -> bool:
             for i, file in enumerate(bookFiles):
 
                 fileName, fileExt = os.path.splitext(file)
-                if settings.config["debug"]>=1:
-                    print(f"DEBUG 1 - RUNNING - Renaming \"{file}\" to \"{title} - part {i + 1 :03d}{fileExt}\"")
+                settings.debug_mes(1, "Running", f"Renaming \"{file}\" to \"{title} - part {i + 1 :03d}{fileExt}\"")
                 os.rename(f"{path}/{file}", f"{path}/{title} - part {i + 1:03d}{fileExt}")
 
         elif len(bookFiles)==1: 
             fileName, fileExt = os.path.splitext(bookFiles[0])
-            if settings.config["debug"]>=1:
-                print(f"DEBUG 1 - RUNNING - Renaming \"{bookFiles[0]}\" to \"{title}{fileExt}\"")
+            settings.debug_mes(1, "Running", f"Renaming \"{bookFiles[0]}\" to \"{title}{fileExt}\"")
             os.rename(f"{path}/{bookFiles[0]}", f"{path}/{title}{fileExt}")
         
-        if settings.config["debug"]>=1:
-            print(f"DEBUG 1 - SUCCESS - Finished renaming audiobook files for \"{title}\"")
+        settings.debug_mes(1, "Success", f"Finished renaming audiobook files for \"{title}\"")
         return True
 
-    if settings.config["debug"]>=1:
-        print(f"DEBUG 1 - FAILURE - Unable to rename audiobook files for \"{title}\"")
+    settings.debug_mes(1, "Failure", f"Unable to rename audiobook files for \"{title}\"")
     return False
 
 # generates the cue file from the mp3s track length
 def generate_cue_from_file(title: str, path: str) -> bool:
-    if settings.config["debug"]>=1:
-        print(f"Debug 1 - RUNNING - Generating cue for \"{title}\"")
+    settings.debug_mes(1, "Running", f"Generating cue for \"{title}\"")
     
     bookFiles = enumerate_audiobook_folder(path)
 
@@ -146,17 +136,15 @@ def generate_cue_from_file(title: str, path: str) -> bool:
                         time = cue_time(chapter.start)
                         f.write(f"  TRACK {n :03d} AUDIO\n    TITLE \"{chapter.title}\"\n    INDEX 01 {time["minutes"] :02d}:{time["seconds"] :02d}:{time["frames"] :5.3f}\n")
 
-            if settings.config["debug"]>=1:
-                print(f"Debug 1 - SUCCESS - Generated cue for \"{title}\"")
+            settings.debug_mes(1, "Success", f"Generated cue for \"{title}\"")
             return True
-    if settings.config["debug"]>=1:
-        print(f"Debug 1 - FAILURE - Unable to enerated cue for \"{title}\"")
+
+    settings.debug_mes(1, "Failure", f"Unable to enerated cue for \"{title}\"")
     return False
 
 # calculates min sec and frames for cue files
 def cue_time(time):
-    if settings.config["debug"]>=2:
-        print(f"Debug 2 - RUNNING - Entering CueTime")
+    settings.debug_mes(2, "Running", "Entering CueTime")
 
     cueTime = {}
     cueTime["minutes"] = int(time // 60)
@@ -167,8 +155,7 @@ def cue_time(time):
 
 # Generates the chapter information for the audiobookshelf metadata
 def generate_cue_for_metadata(title: str, path: str) -> dict:
-    if settings.config["debug"]>=1:
-        print(f"Debug 1 - RUNNING - Generating chapter times for metadata")
+    settings.debug_mes(1, "Running", "Generating chapter times for metadata")
     chapters = []
 
     bookFiles = enumerate_audiobook_folder(path)
@@ -221,7 +208,7 @@ def generate_cue_for_metadata(title: str, path: str) -> dict:
             totalTime = 0
 
             mp3 = MP3(f"{path}/{file}")
-            if i==len(bookFilesMP3)+1:
+            if i==len(bookFiles)+1:
                 previousChapter["end"] = round(totalTime, 3)
                 chapters.append(previousChapter)
                 finalChapter = {
@@ -255,16 +242,18 @@ def generate_cue_for_metadata(title: str, path: str) -> dict:
             totalTime += float(mp3.info.length)
                     
 
-    if settings.config["debug"]>=1:
-        print(f"Debug 1 - SUCCESS - Generating chapter times for metadata") 
+    settings.debug_mes(1, "Success", "Generating chapter times for metadata")
     return chapters
     
     return False
 
+# Lists 
 def enumerate_audiobook_folder(path: str) -> list:
     bookFiles = []
+
     for file in os.listdir(path):
         fileName, fileExt = os.path.splitext(file)
         if fileExt.lower()==".mp3" or fileExt.lower()==".m4b":
             bookFiles.append(file)
+
     return bookFiles
