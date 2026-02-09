1b5a45550870329ef05934814897d9fed855148b
Roman <roman.c.antonacci@gmail.com>
2026-02-08 20:12:00 -0500

    Renamed Changed Echo to sys_mes Function.

--- utils/account.py
+++ utils/account.py
@@ -3,19 +3,17 @@ import json, os
 
 # Gets accounts from an account.json file OR from environment variables
 def set_accounts(parsedAccount):
-    if settings.config["debug"]>=1:
-        print("DEBUG 1 - RUNNING - Finding Libro.FM accounts.")
+    settings.debug_mess(1, "RUNNING", "Finding Libro.FM accounts.")
 
     accounts = {}
 
     if (os.path.exists(f"{settings.config["account_dir"]}/{settings.config["account_file"]}")):
-        if settings.config["debug"]>=1:
-            print("DEBUG 1 - SUCCESS - Account.json file found; setting accounts from JSON.")
+        settings.debug_mess(1, SUCCESS, f"{settings.config["account_file"]} file found; setting accounts from JSON.")
 
         with open(f"{settings.config["account_dir"]}{settings.config["account_file"]}", "r") as file:
             accounts = json.loads(file.read())
-        if settings.config["debug"]==2:
-            print(f"DEBUG 2 - RUNNING - Loaded {len(accounts)} accounts from file.")
+        
+        settings.debug_mess(2, "RUNNING", f"Loaded {len(accounts)} accounts from file.")
 
         # Adds any environment accounts that were not inclued in the json. Libro-Tea reads accounts as if they in a json format regardless of where they came from.
         envAccounts = set_accounts_from_env_var(len(accounts))
@@ -35,11 +33,9 @@ def set_accounts(parsedAccount):
 
     return accounts
 
-
 # Gets account infromation from the environment variables; breaks out Secrets when found
 def set_accounts_from_env_var(numAccounts: int):
-    if settings.config["debug"]>=1:
-        print("DEBUG 1 - RUNNING - Looking for Libro.FM accounts in environment")
+    settings.debug_mess(1, "RUNNING", "Looking for Libro.FM accounts in environment")
 
     user = []
     password = []
